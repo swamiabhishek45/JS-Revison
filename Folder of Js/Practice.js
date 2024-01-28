56a90e36074bf4fe3a59e30d7449eaf8845f5e35
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-01-28 18:15:43 +0530

    Todo Revise

--- Folder of Js/Practice.js
+++ Folder of Js/Practice.js
@@ -1,14 +1,35 @@
-const body = document.querySelector("body");
+const todoForm = document.querySelector(".form-todo");
+const todoInput = document.querySelector(".form-todo input[type='text']");
+const todoList = document.querySelector(".todo-list");
 
-// body.addEventListener("keypress", (e) =>{
-//     console.log(e.key);
-// })
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (todoInput.value === "") {
+    console.log("Add some Todo")
+  } else {
+    const newTodoText = todoInput.value;
+    const newLi = document.createElement("li");
+    const newLiInnerHtml = `<span class="text">${newTodoText}</span>
+          <div class="todo-btns">
+            <button class="todo-btn done">Done</button>
+            <button class="todo-btn remove">Remove</button>
+          </div>
+        `;
 
-const btn = document.querySelector("button");
+    newLi.innerHTML = newLiInnerHtml;
+    todoList.append(newLi);
+    todoInput.value = "";
+  }
+});
 
-btn.addEventListener("mouseover", (e) =>{
-    console.log("Mouse over event occur");
-})
-btn.addEventListener("mouseleave", (e) =>{
-    console.log("Mouse leave event occur");
-})
+todoList.addEventListener("click", (e) => {
+  // check if user clicked on user button
+  if (e.target.classList.contains("done")) {
+    const spanLi = e.target.parentNode.previousElementSibling;
+    spanLi.style.textDecoration = "Line-through";
+  }
+  if (e.target.classList.contains("remove")) {
+    const targetedLi = e.target.parentNode.parentNode;
+    targetedLi.remove();
+  }
+});
--- Folder of Js/new.js
+++ Folder of Js/new.js
@@ -5,34 +5,43 @@ const parent = document.querySelector(".parent");
 const child = document.querySelector(".child");
 
 // Capturing
-child.addEventListener("click", ()=>{
-  console.log("Child Captured");
-  console.log("**********************");
-}, true)
+// child.addEventListener("click", ()=>{
+//   console.log("Child Captured");
+//   console.log("**********************");
+// }, true)
 
-parent.addEventListener("click", ()=>{
-  console.log("Parent Captured");
-}, true)
+// parent.addEventListener("click", ()=>{
+//   console.log("Parent Captured");
+// }, true)
 
-grandParent.addEventListener("click", ()=>{
-  console.log("Grandparent Captured");
-}, true)
+// grandParent.addEventListener("click", ()=>{
+//   console.log("Grandparent Captured");
+// }, true)
 
-document.body.addEventListener("click", ()=>{
-  console.log("Body Captured");
-}, true)
+// document.body.addEventListener("click", ()=>{
+//   console.log("Body Captured");
+// }, true)
 
 // Bubbling
-child.addEventListener("click", ()=>{
-  console.log("Child Clicked 👶");
-})
-parent.addEventListener("click", ()=>{
-  console.log("Parent Clicked 💏");
-})
-grandParent.addEventListener("click", ()=>{
-  console.log("Grandparent Clicked 👴");
-})
+// child.addEventListener("click", ()=>{
+//   console.log("Child Clicked 👶");
+// })
+// parent.addEventListener("click", ()=>{
+//   console.log("Parent Clicked 💏");
+// })
+// grandParent.addEventListener("click", ()=>{
+//   console.log("Grandparent Clicked 👴");
+// })
 
-document.body.addEventListener("click", ()=>{
-  console.log("Body Clicked");
+// document.body.addEventListener("click", ()=>{
+//   console.log("Body Clicked");
+// })
+
+
+
+// event delegation
+
+grandParent.addEventListener("click", (e) =>{
+  // console.dir(e.currentTarget);
+  console.dir(e.target);
 })
