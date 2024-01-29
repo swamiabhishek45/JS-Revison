3b9dd19511aa44e0e47b33f11452f63fec1a8015
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-01-29 14:47:42 +0530

    Revise pyramid of doom

--- Folder of Js/new.js
+++ Folder of Js/new.js
@@ -1,29 +1,37 @@
-const body = document.querySelector("body")
-const startBtn = document.querySelector("#start");
-const stopBtn = document.querySelector("#stop");
+const heading1 = document.querySelector(".heading-1");
+const heading2 = document.querySelector(".heading-2");
+const heading3 = document.querySelector(".heading-3");
+const heading4 = document.querySelector(".heading-4");
+const heading5 = document.querySelector(".heading-5");
+const heading6 = document.querySelector(".heading-6");
+const heading7 = document.querySelector(".heading-7");
 
-function randomColorGenerator(){
-    const red = Math.floor(Math.random() *256);
-    const green = Math.floor(Math.random() *256);
-    const yellow = Math.floor(Math.random() *256);
+setTimeout(() => {
+  heading1.textContent = "One"
+  heading1.style.color = "Violet"
+  setTimeout(() => {
+    heading2.textContent = "Two";
+    heading2.style.color = "purple";
+    setTimeout(() => {
+      heading3.textContent = "Three";
+      heading3.style.color = "red";
+      setTimeout(() => {
+        heading4.textContent = "Four";
+        heading4.style.color = "pink";
+        setTimeout(() => {
+          heading5.textContent = "Five";
+          heading5.style.color = "green";
+          setTimeout(() => {
+            heading6.textContent = "Six";
+            heading6.style.color = "blue";
+            setTimeout(() => {
+              heading7.textContent = "Seven";
+              heading7.style.color = "browen";
+            }, 1000);
+          }, 3000);
+        }, 2000);
+      }, 1000);
+    }, 2000);
+  }, 2000);
+}, 1000);
 
-    const randomNum = `rgb(${red}, ${green}, ${yellow})`;
-
-    return randomNum;
-}
-
-startBtn.addEventListener("click", () => {
-  function color() {
-    const random = randomColorGenerator();
-    body.style.backgroundColor = random;
-  }
-  const id = setInterval(color, 1000);
-});
-
-stopBtn.addEventListener("click", () => {
- 
-    body.style.backgroundColor = "";
-    clearInterval(id);
-  
-  
-});
