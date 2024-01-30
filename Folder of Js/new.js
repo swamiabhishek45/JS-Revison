4a0ceff81136931885081c2fee104da2303abefc
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-01-30 22:57:53 +0530

    today's changes done

--- Folder of Js/new.js
+++ Folder of Js/new.js
@@ -2,18 +2,18 @@ const key1 = "obj1";
 const key2 = "obj2";
 
 const obj1 = {
-    key: "val1"
-}
+  key: "val1",
+};
 
 const obj2 = {
-    key1: "val2"
-}
+  key1: "val2",
+};
 
-const cloneObj = {...obj1, ...obj2};
+const cloneObj = { ...obj1, ...obj2 };
 
 console.log(cloneObj);
 
-const arr1 = [1,2,34,4];
-const arr2 = [1,'hi', 4,3,2];
+const arr1 = [1, 2, 34, 4];
+const arr2 = [1, "hi", 4, 3, 2];
 
 console.log([...arr1, ...arr2]);
