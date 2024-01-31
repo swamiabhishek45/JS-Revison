a5b427333c2d6371ac93d446d37af9a7973ef1dd
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-01-31 12:51:59 +0530

    changes

--- Folder of Js/new.js
+++ Folder of Js/new.js
@@ -1,19 +1,18 @@
-const key1 = "obj1";
-const key2 = "obj2";
 
-const obj1 = {
-  key: "val1",
+const URL = "https://jsonplaceholder.typicode.com/posts";
+const xhr = new XMLHttpRequest();
+
+
+xhr.open('GET', URL);
+console.log(xhr.readyState);
+console.log(xhr.readyState);
+
+xhr.onload = function () {
+  const response = xhr.response; // string
+  const data = JSON.parse(response);
+  console.log(data); // object
 };
 
-const obj2 = {
-  key1: "val2",
-};
 
-const cloneObj = { ...obj1, ...obj2 };
 
-console.log(cloneObj);
-
-const arr1 = [1, 2, 34, 4];
-const arr2 = [1, "hi", 4, 3, 2];
-
-console.log([...arr1, ...arr2]);
+xhr.send();
