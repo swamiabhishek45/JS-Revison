7daafa7741f835d0116ca1bbfbd10c07ed937f78
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-01-31 13:32:56 +0530

    XMLHttpRequest Done

--- Folder of Js/new.js
+++ Folder of Js/new.js
@@ -1,18 +1,43 @@
-
 const URL = "https://jsonplaceholder.typicode.com/posts";
-const xhr = new XMLHttpRequest();
 
+function sendRequest(method, url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open('GET', URL);
-console.log(xhr.readyState);
-console.log(xhr.readyState);
+    xhr.open(method, url);
 
-xhr.onload = function () {
-  const response = xhr.response; // string
-  const data = JSON.parse(response);
-  console.log(data); // object
-};
+    xhr.onload = () => {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject(new Error(xhr.status));
+      }
+    };
 
+    xhr.onerror = () => {
+      new Error("somehing went wrong");
+    };
 
+    xhr.send();
+  });
+}
 
-xhr.send();
+sendRequest("GET", URL)
+  .then((response) => {
+    const data = JSON.parse(response);
+    return data;
+  })
+  .then((data) => {
+    const id = data[3].id;
+    return id;
+  })
+  .then((id) => {
+    const url = `${URL}/${id}`;
+    return sendRequest("GET", url);
+  })
+  .then((newResponse) => {
+    const newData = JSON.parse(newResponse);
+    console.log(newData);
+  }).catch(error=>{
+    console.log(error);
+  })
