6a7b5b8156191c5d5f2cbe58ec05c619db7d6d83
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-04 18:36:54 +0530

    changes

--- Folder of Js/new.js
+++ Folder of Js/new.js
@@ -1,4 +1,41 @@
-const myArray = ["apple", "banana", "orange"];
+// singleton
 
-const myList = myArray.map((item) => item);
-console.log(myList); 
+// object literals
+
+const mysym = Symbol();
+
+const user = {
+  [mysym]: 1,
+  name: "Abhishek",
+  age: 21,
+  location: "Latur",
+  email: "abhi@gmail.com",
+  isLoggedIn: false,
+  lastLOgindays: ["Monday", "Sunday"],
+  greet: () =>{
+    return "Inside OBJ";
+    
+  }
+};
+
+console.log(typeof user[mysym]);
+
+console.log(user.name);
+console.log(user["age"]);
+
+console.log(user);
+
+user.id = 34;
+
+console.log(user.id);
+
+console.log(user.greet());
+
+user.greet = () =>{
+    console.log("Hello Js");
+    
+}
+
+
+user.greet();
+
