3ae2b49879f3e791c396c9a28acc38b77e2f80f9
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-26 20:10:07 +0530

    revise array methods

--- Folder of Js/test.js
+++ Folder of Js/test.js
@@ -1,26 +1,38 @@
-const bucket = ["coffee", "chips", "vegetables", "rice"];
+// const bucket = ["coffee", "chips", "vegetables", "rice"];
 
-const friedRicePromise = new Promise((resolve, reject) => {
-  if (
-    bucket.includes("salt") &&
-    bucket.includes("vegetables") &&
-    bucket.includes("rice")
-  ) {
-    resolve("Fried Rice");
-  } else {
-    reject(new Error("Something went wrong"));
-  }
-});
+// const friedRicePromise = new Promise((resolve, reject) => {
+//   if (
+//     bucket.includes("salt") &&
+//     bucket.includes("vegetables") &&
+//     bucket.includes("rice")
+//   ) {
+//     resolve("Fried Rice");
+//   } else {
+//     reject(new Error("Something went wrong"));
+//   }
+// });
 
-// consume Promise
+// // consume Promise
 
-friedRicePromise
-  .then(
-    // when promise resolve
-    (myFriedRice) => {
-      console.log("Lets eat", myFriedRice);
-    }
-  )
-  .catch((error) => {
-    console.log(error);
-  });
+// friedRicePromise
+//   .then(
+//     // when promise resolve
+//     (myFriedRice) => {
+//       console.log("Lets eat", myFriedRice);
+//     }
+//   )
+//   .catch((error) => {
+//     console.log(error, "error message");
+//   });
+
+const arr = [1,2,3,4,5,6,7,8,9,10]
+console.log("Original Array", arr);
+
+
+const pushed = arr.unshift(11)
+const poped = arr.shift();
+
+console.log("Pushed", pushed);
+console.log("poped", poped);
+
+console.log("New Array", arr);
