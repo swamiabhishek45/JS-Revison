28d9949028723a7b4f567b9bca1caf4105cbc25b
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-26 23:07:01 +0530

    practice

--- Folder of Js/test.js
+++ Folder of Js/test.js
@@ -25,14 +25,25 @@
 //     console.log(error, "error message");
 //   });
 
-const arr = [1,2,3,4,5,6,7,8,9,10]
-console.log("Original Array", arr);
+// const arr = [1,2,3,4,5,6,7,8,9,10]
+// console.log("Original Array", arr);
 
 
-const pushed = arr.unshift(11)
-const poped = arr.shift();
+// const pushed = arr.unshift(11)
+// const poped = arr.shift();
 
-console.log("Pushed", pushed);
-console.log("poped", poped);
+// console.log("Pushed", pushed);
+// console.log("poped", poped);
 
-console.log("New Array", arr);
+// console.log("New Array", arr);
+
+
+const arr = [1,2,3,4,5,6,7,];
+console.log(arr);
+
+const newArr = [...arr];
+console.log(newArr);
+
+console.log(arr === newArr);
+
+
