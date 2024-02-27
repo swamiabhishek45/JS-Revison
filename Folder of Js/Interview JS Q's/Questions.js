313a498b147833085c19884db3c5ba4c1cba2425
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-27 23:05:42 +0530

    Question 1

---
+++ Folder of Js/Interview JS Q's/Questions.js
@@ -0,0 +1,12 @@
+var arr = [1,2,3,4,5,6,7,8,9,10];
+
+// forEach is a loop which works on arrays 
+
+// (higher order function) => function inside function
+
+// Question 1: Print hello infront of each array value
+arr.forEach((val) => {
+    console.log(val + "Hello");
+})
+
+// console.log(arr);
