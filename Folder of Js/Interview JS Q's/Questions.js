7f2d8762041a98f38b32dea04d16753c66b2da6c
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-02-27 23:07:42 +0530

    Question 2

--- Folder of Js/Interview JS Q's/Questions.js
+++ Folder of Js/Interview JS Q's/Questions.js
@@ -5,8 +5,14 @@ var arr = [1,2,3,4,5,6,7,8,9,10];
 // (higher order function) => function inside function
 
 // Question 1: Print hello infront of each array value
-arr.forEach((val) => {
-    console.log(val + "Hello");
-})
+// arr.forEach((val) => {
+//     console.log(val + "Hello");
+// })
 
+// Question 2: Print val less than 3
+arr.forEach(function(val){
+    if(val < 3) {
+        console.log(val);   
+    }
+})
 // console.log(arr);
