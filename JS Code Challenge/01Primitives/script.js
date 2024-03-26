ab8f037607dc904382ef409c6d1349fd91f03ab2
swamiabhishek45 <abhishekswami1435@gmail.com>
2024-03-26 10:26:50 +0530

    typeof operator

---
+++ JS Code Challenge/01Primitives/script.js
@@ -0,0 +1,32 @@
+/*
+// typeof operator --> returns a string indicating the type of operand.
+
+console.log(typeof 50); // number
+console.log(typeof "text"); // string
+console.log(typeof true); // boolean
+console.log(typeof undefined); // undefined
+console.log(typeof function(){}); // function
+console.log(typeof 10n); // bigint
+console.log(typeof Symbol()); // symbol
+console.log(typeof [1,2,3]); // object
+console.log(typeof {}); // object
+
+console.log(typeof NaN); // number --> (NaN is not a number)
+console.log(typeof undeclaredVar); // undefined --> (undeclaredVariable is never declared)
+console.log(typeof Infinity); // number --> (Infinity, -Infinity, -0 are all valid numbers in JavaScript)
+console.log(typeof null); // object --> (This stands since beginning of JavaScript)
+console.log(typeof /regex/); // object --> (regular expression start and end with '/' in literal form)
+console.log(typeof typeof 1);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
